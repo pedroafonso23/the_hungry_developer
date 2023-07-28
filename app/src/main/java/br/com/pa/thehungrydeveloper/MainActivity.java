b24257b4b22ff16f38ab9c150b77e535aa7b75dc
Pedro Afonso <pedroafonso23@yahoo.com.br>
2023-07-28 19:00:31 -0300

    main and starters cards

--- app/src/main/java/br/com/pa/thehungrydeveloper/MainActivity.java
+++ app/src/main/java/br/com/pa/thehungrydeveloper/MainActivity.java
@@ -1,14 +1,21 @@
 package br.com.pa.thehungrydeveloper;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
 public class MainActivity extends AppCompatActivity {
+
+    CardView startersCard;
+    CardView mainsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startersCard = findViewById(R.id.card_view_starters);
+        mainsCard = findViewById(R.id.card_view_mains);
     }
 }
